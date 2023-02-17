64fdaf51e5cf15df9b9acaa93b3f68d1839bd221
Lucasdaniel314 <lucasdanielmonteirodesousa827@gmail.com>
2023-02-17 15:00:14 -0300

    criado pacote xadrez e a classe UI

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,11 +1,12 @@
 package aplicacao;
 
-import camadaTabuleiro.Tabuleiro;
+import xadrez.PartidaXadrez;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+		PartidaXadrez partidaXadrez = new PartidaXadrez();
+		UI.imprimeTabuleiro(partidaXadrez.getPecas());
 	}
 
 }
