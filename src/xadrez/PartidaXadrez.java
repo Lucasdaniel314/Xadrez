3808a20dd1f14918bf69f26949e5f22bd96531e3
Lucasdaniel314 <lucasdanielmonteirodesousa827@gmail.com>
2023-02-18 11:43:43 -0300

    aplicando cores no tabuleiro

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -23,14 +23,23 @@ public class PartidaXadrez {
 		return matriz;
 	}
 	
-	private void posicaoNovaPeca(char coluna, int linha, PecaXadrez peca) {
+	private void posicaoXadrez(char coluna, int linha, PecaXadrez peca) {
 		tabuleiro.posicaoPeca(peca, new PosicaoXadrez(coluna, linha).toPosicao());
 	}
 	
 	private void partidaInicial() {
-		posicaoNovaPeca('a' , 6, new Torre(tabuleiro, Cor.preto));
-		posicaoNovaPeca('h', 5 , new Torre(tabuleiro, Cor.branco));
-		posicaoNovaPeca('e', 1, new Rei(tabuleiro, Cor.preto));
-		posicaoNovaPeca('e', 8, new Rei(tabuleiro, Cor.branco));
+		posicaoXadrez('c', 1, new Torre(tabuleiro, Cor.branco));
+		posicaoXadrez('c', 2, new Torre(tabuleiro, Cor.branco));
+		posicaoXadrez('d', 2, new Torre(tabuleiro, Cor.branco));
+		posicaoXadrez('e', 2, new Torre(tabuleiro, Cor.branco));
+		posicaoXadrez('e', 1, new Torre(tabuleiro, Cor.branco));
+		posicaoXadrez('d', 1, new Rei(tabuleiro, Cor.branco));
+
+		posicaoXadrez('c', 7, new Torre(tabuleiro, Cor.preto));
+		posicaoXadrez('c', 8, new Torre(tabuleiro, Cor.preto));
+		posicaoXadrez('d', 7, new Torre(tabuleiro, Cor.preto));
+		posicaoXadrez('e', 7, new Torre(tabuleiro, Cor.preto));
+		posicaoXadrez('e', 8, new Torre(tabuleiro, Cor.preto));
+		posicaoXadrez('d', 8, new Rei(tabuleiro, Cor.preto));
 	}
 }
